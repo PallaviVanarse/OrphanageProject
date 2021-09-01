9eb1beb336239830efcd0947ae68ad13da9ba5aa
PallaviVanarse <pallavivanarse27@gmail.com>
2021-09-01 15:21:31 +0530

    Initial commit to push changes

---
+++ FirstDemo/FirstDemo/Controllers/orphanageRegistration1Controller.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FirstDemo.Models;
+
+namespace FirstDemo.Controllers
+{
+    public class orphanageRegistration1Controller : ApiController
+    {
+        private ActionLearningEntities db = new ActionLearningEntities();
+
+        // GET: api/orphanageRegistration1
+        public IQueryable<orphanageRegistration1> GetorphanageRegistration1()
+        {
+            return db.orphanageRegistration1;
+        }
+
+        // GET: api/orphanageRegistration1/5
+        [ResponseType(typeof(orphanageRegistration1))]
+        public IHttpActionResult GetorphanageRegistration1(int id)
+        {
+            orphanageRegistration1 orphanageRegistration1 = db.orphanageRegistration1.Find(id);
+            if (orphanageRegistration1 == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(orphanageRegistration1);
+        }
+
+        // PUT: api/orphanageRegistration1/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutorphanageRegistration1(int id, orphanageRegistration1 orphanageRegistration1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != orphanageRegistration1.oId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(orphanageRegistration1).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!orphanageRegistration1Exists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/orphanageRegistration1
+        [ResponseType(typeof(orphanageRegistration1))]
+        public IHttpActionResult PostorphanageRegistration1(orphanageRegistration1 orphanageRegistration1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.orphanageRegistration1.Add(orphanageRegistration1);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = orphanageRegistration1.oId }, orphanageRegistration1);
+        }
+
+        // DELETE: api/orphanageRegistration1/5
+        [ResponseType(typeof(orphanageRegistration1))]
+        public IHttpActionResult DeleteorphanageRegistration1(int id)
+        {
+            orphanageRegistration1 orphanageRegistration1 = db.orphanageRegistration1.Find(id);
+            if (orphanageRegistration1 == null)
+            {
+                return NotFound();
+            }
+
+            db.orphanageRegistration1.Remove(orphanageRegistration1);
+            db.SaveChanges();
+
+            return Ok(orphanageRegistration1);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool orphanageRegistration1Exists(int id)
+        {
+            return db.orphanageRegistration1.Count(e => e.oId == id) > 0;
+        }
+    }
+}
---
+++ FirstDemo/FirstDemo/Controllers/orphanageRegistrationMVCController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FirstDemo.Models;
+
+namespace FirstDemo.Controllers
+{
+    public class orphanageRegistrationMVCController : Controller
+    {
+        private ActionLearningEntities db = new ActionLearningEntities();
+
+        // GET: orphanageRegistrationMVC
+        public ActionResult Index()
+        {
+            return View(db.orphanageRegistration1.ToList());
+        }
+
+        // GET: orphanageRegistrationMVC/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            orphanageRegistration1 orphanageRegistration1 = db.orphanageRegistration1.Find(id);
+            if (orphanageRegistration1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orphanageRegistration1);
+        }
+
+        // GET: orphanageRegistrationMVC/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: orphanageRegistrationMVC/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "oId,oName,oRegistrationNum,addressLine1,addressLine2,landmark,city,state,country,pincode,phoneNum,password")] orphanageRegistration1 orphanageRegistration1)
+        {
+            if (ModelState.IsValid)
+            {
+                db.orphanageRegistration1.Add(orphanageRegistration1);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(orphanageRegistration1);
+        }
+
+        // GET: orphanageRegistrationMVC/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            orphanageRegistration1 orphanageRegistration1 = db.orphanageRegistration1.Find(id);
+            if (orphanageRegistration1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orphanageRegistration1);
+        }
+
+        // POST: orphanageRegistrationMVC/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "oId,oName,oRegistrationNum,addressLine1,addressLine2,landmark,city,state,country,pincode,phoneNum,password")] orphanageRegistration1 orphanageRegistration1)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(orphanageRegistration1).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(orphanageRegistration1);
+        }
+
+        // GET: orphanageRegistrationMVC/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            orphanageRegistration1 orphanageRegistration1 = db.orphanageRegistration1.Find(id);
+            if (orphanageRegistration1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orphanageRegistration1);
+        }
+
+        // POST: orphanageRegistrationMVC/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            orphanageRegistration1 orphanageRegistration1 = db.orphanageRegistration1.Find(id);
+            db.orphanageRegistration1.Remove(orphanageRegistration1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
